9cbdfd1b9764655a0bd27e8ccfc06db11b3eb843
o3ojunseok <o3ojunseok@gmail.com>
2024-10-09 00:44:27 +0900

    Feat: repository find, findAll

--- src/content/content.module.ts
+++ src/content/content.module.ts
@@ -3,12 +3,18 @@ import { ContentService } from "./content.service";
 import { ContentController } from "./content.controller";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { Content } from "./content.entity";
+import { ContentRepository } from "./content.repository";
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([Content])
   ],
-  controllers: [ContentController],
-  providers: [ContentService],
+  controllers: [
+    ContentController,
+  ],
+  providers: [
+    ContentService,
+    ContentRepository
+  ],
 })
 export class ContentModule {}
--- src/content/content.repository.ts
+++ src/content/content.repository.ts
@@ -3,8 +3,18 @@ import { DataSource, Repository } from "typeorm";
 import { Content } from "./content.entity";
 
 @Injectable()
-export class ProjectRepository extends Repository<Content> {
+export class ContentRepository extends Repository<Content> {
   constructor(private dataSource: DataSource) {
     super(Content, dataSource.createEntityManager());
   }
+
+  async findAll() {
+    return await this.find();
+  }
+
+  async findOneContent(id: number) {
+    return await this.findOne({
+      where: { id },
+    });
+  } 
 }
