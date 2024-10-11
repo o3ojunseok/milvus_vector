1516732514676c1d4056749b8d03914e3d8a163d
o3ojunseok <o3ojunseok@gmail.com>
2024-10-11 23:41:23 +0900

    Feat: updateContentDto

--- src/content/dto/update-content.dto.ts
+++ src/content/dto/update-content.dto.ts
@@ -1,4 +1,13 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateContentDto } from './create-content.dto';
+import { IsOptional, IsString } from 'class-validator';
 
-export class UpdateContentDto extends PartialType(CreateContentDto) {}
+export class UpdateContentDto extends PartialType(CreateContentDto) {
+  @IsString()
+  @IsOptional()
+  content: string;
+
+  @IsString()
+  @IsOptional()
+  answer: string;
+}
