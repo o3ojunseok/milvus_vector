a3fc047c9746bca9b8a86dc3420439266f43042c
o3ojunseok <o3ojunseok@gmail.com>
2024-10-22 20:13:11 +0900

    Feat: health check

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,33 @@
 import { Controller, Get } from "@nestjs/common";
 import { AppService } from "./app.service";
+import { HealthCheck, HealthCheckService, HttpHealthIndicator, MemoryHealthIndicator, TypeOrmHealthIndicator } from "@nestjs/terminus";
+import { InjectDataSource } from "@nestjs/typeorm";
+import { DataSource } from "typeorm";
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    private health: HealthCheckService,
+    private http: HttpHealthIndicator,
+    private db: TypeOrmHealthIndicator,
+    private memory: MemoryHealthIndicator,
+    @InjectDataSource()
+    private defaultDataSource: DataSource,
+  ) {}
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get("health")
+  @HealthCheck()
+  check() {
+      return this.health.check([
+          () => this.db.pingCheck("database", { connection: this.defaultDataSource }),
+          () => this.memory.checkHeap("memory heap", 300 * 1024 * 1024),
+          () => this.memory.checkRSS("memory RSS", 300 * 1024 * 1024),
+      ])
   }
 }
