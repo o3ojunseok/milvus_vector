0062853f2748bb6095cd85f2d588c952fda7a2cf
o3ojunseok <o3ojunseok@gmail.com>
2024-10-25 15:59:10 +0900

    Feat: globalpipes and enable cors

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,25 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
+import { ValidationPipe } from "@nestjs/common";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
+
+  app.enableCors({
+    origin: true,
+    methods: 'GET,HEAD,POST,PUT,DELETE',
+    credentials: true,
+  });
+  
   const config = new DocumentBuilder()
     .setTitle("Milvus")
     .setDescription("Milvus API")
