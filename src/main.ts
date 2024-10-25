8d9a2c00f0703a5ff3d30d383826cdc3083aef59
o3ojunseok <o3ojunseok@gmail.com>
2024-10-26 07:23:10 +0900

    Feat: main.ts logger

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,11 @@ import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 import { ValidationPipe } from "@nestjs/common";
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule,
+    {
+      logger: ["error", "log", "debug"],
+    }
+  );
 
   app.useGlobalPipes(
     new ValidationPipe({
