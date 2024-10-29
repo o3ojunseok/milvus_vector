f7f858f1c5cb776a638487a3666365a6d8dbb78d
o3ojunseok <o3ojunseok@gmail.com>
2024-10-29 22:13:13 +0900

    Fix: main

--- src/main.ts
+++ src/main.ts
@@ -30,6 +30,7 @@ async function bootstrap() {
     .setVersion("1.0")
     .addTag("Milvus")
     .build();
+    
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup("api", app, document);
 
