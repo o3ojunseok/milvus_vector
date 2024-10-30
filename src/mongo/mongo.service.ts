eea1b2e0f3e00dc376d274232c232729da9bedf5
o3ojunseok <o3ojunseok@gmail.com>
2024-10-30 12:49:03 +0900

    Fix: remove import

--- src/mongo/mongo.service.ts
+++ src/mongo/mongo.service.ts
@@ -1,6 +1,4 @@
 import { Injectable } from "@nestjs/common";
-import { CreateMongoDto } from "./dto/create-mongo.dto";
-import { UpdateMongoDto } from "./dto/update-mongo.dto";
 
 @Injectable()
 export class MongoService {
